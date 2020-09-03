244478cd445e94a32f6df503a9c70ad4663174c5
Robert <deathpad17@gmail.com>
2020-09-03 22:39:06 +0700

    Minor update
    Add default assets
    Import Newtonsoft
    Add LevelExporter

---
+++ Assets/Scripts/Datas/PlayerData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ProgrammingBatch.Magnetize.Data
+{
+    public sealed class PlayerData
+    {
+        public Vector3 position;
+        public RotationData rotation;
+    }
+}
---
+++ Assets/Scripts/Datas/WallData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace ProgrammingBatch.Magnetize.Data
+{
+    public sealed class WallData
+    {
+        public Vector3 position;
+        public RotationData rotation;
+        public Vector3 size;
+    }
+}
